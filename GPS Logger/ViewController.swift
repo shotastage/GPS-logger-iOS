701d815384cc2ffaab8e0025a5e8e68c2b2f09a7
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-05 12:45:41 +0900

    Initial Commit

---
+++ GPS Logger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPS Logger
+//
+//  Created by Shota Shimazu on 2020/02/05.
+//  Copyright © 2020 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
